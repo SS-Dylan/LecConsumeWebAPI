691112159dfb7437029e7471bfe19465e22b4254
Dylan Shaffer <13596307+WRX-Dylan@users.noreply.github.com>
2022-09-19 11:39:04 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LecConsumeWebAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
